bba48dfb97d7b73f7f8312adfdca40a5c2f997be
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2021-06-11 23:43:22 +0200

    Player controller movement

---
+++ BallScape/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Variables")]
+    [SerializeField]
+    float speed = 5.0f;
+
+    Rigidbody2D rb;
+    SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W)) { direction.y += 1; }
+        if (Input.GetKey(KeyCode.S)) { direction.y -= 1; }
+
+        if (Input.GetKey(KeyCode.A)) { direction.x -= 1; }
+        if (Input.GetKey(KeyCode.D)) { direction.x += 1; }
+
+        if (direction != Vector2.zero)
+        {
+            rb.position += direction.normalized * Time.fixedDeltaTime * speed;
+            rb.rotation = Vector2.SignedAngle(Vector2.down, direction.normalized);
+        }
+    }
+}
