ade50035dacdc04043f0edb13ec6ae58f62430b9
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2021-06-12 00:50:54 +0200

    Scene and lights

---
+++ BallScape/Assets/Scripts/Alarm.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+    [SerializeField]
+    float rotationSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(0, 0, rotationSpeed * Time.deltaTime);
+    }
+}
