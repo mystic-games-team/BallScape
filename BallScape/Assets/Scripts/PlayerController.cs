208426ef19b0bb489c298884bd24b0ae04064921
Oriol Capdevila <36154523+OriolCS2@users.noreply.github.com>
2021-06-12 11:29:27 +0200

    F

--- BallScape/Assets/Scripts/PlayerController.cs
+++ BallScape/Assets/Scripts/PlayerController.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Variables")]
+    [Header("Movement")]
     [SerializeField]
     float speed = 5.0f;
 
+    [Header("Ball")]
+    [SerializeField] Rigidbody2D ball;
+    [SerializeField] float force = 10.0F;
+
     Rigidbody2D rb;
     SpriteRenderer sprite;
+    Camera camera;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        camera = Camera.allCameras[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 worldPoint = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y));
+            Vector2 direction = new Vector2(worldPoint.x, worldPoint.y) - ball.position;
+            ball.AddForce(direction.normalized * force, ForceMode2D.Force);
+        }
     }
 
     private void FixedUpdate()
